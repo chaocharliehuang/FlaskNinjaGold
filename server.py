f580e6722c46760b061f399e188934369255cf2c
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-07-06 15:36:41 -0700

    going to a different building adds/subtracts gold, activity gets printed

--- server.py
+++ server.py
@@ -1,14 +1,44 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, request, redirect
+import random
 
 app = Flask(__name__)
 app.secret_key = 'secret'
 
+
 @app.route('/')
 def index():
+    if session.get('gold') == None and session.get('activities') == None:
+        session['gold'] = 0
+        session['activities'] = ''
+    
     return render_template('index.html')
+
 
 @app.route('/process_money', methods=['POST'])
 def process():
-    pass
+    buildingVisited = request.form['building']
+    if buildingVisited == 'farm':
+        dGold = random.randrange(10, 21)
+    elif buildingVisited == 'cave':
+        dGold = random.randrange(5, 11)
+    elif buildingVisited == 'house':
+        dGold = random.randrange(2, 6)
+    elif buildingVisited == 'casino':
+        dGold = random.randrange(-50, 51)
+    session['gold'] += dGold
+    if session['gold'] < 0:
+        session['gold'] = 0
+
+    activitiesStr = ''
+    if dGold >= 0:
+        activitiesStr += 'Earned '
+    else:
+        activitiesStr += 'Lost '
+    activitiesStr += str(abs(dGold)) + ' gold(s) '
+    activitiesStr += 'from the ' + buildingVisited + '!\n'
+    session['activities'] = activitiesStr + session['activities']
+
+    return redirect('/')
+
 
 app.run(debug=True)
